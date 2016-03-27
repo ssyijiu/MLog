6297d4aa037b3d3cd247bba6971dd407131358ff
lxm <lxmyijiu@126.com>
2016-03-27 22:15:54 +0800

    init

---
+++ MLog.java
@@ -0,0 +1,231 @@
+package com.ssyijiu.mlog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Log工具，将xUtils中LogUtils提取成一个java类，整合了KLog中打印josn数据功能
+ * 
+ * tag自动产生，格式: customTagPrefix:className.methodName(L:lineNumber),
+ * customTagPrefix 为空时只输出：className.methodName(L:lineNumber)
+ * 
+ * Author: ssyijiu 
+ * Date: 16-3-27 
+ * Time: 上午9:05
+ */
+public class MLog {
+
+	public static String customTagPrefix = "";
+
+	private MLog() {
+	}
+
+	public static boolean allowE = true;
+	public static boolean allowW = true;
+	public static boolean allowI = true;
+	public static boolean allowD = true;
+	public static boolean allowV = true;
+	public static boolean allowWtf = true;
+	public static boolean allowJson = true;
+	
+	public static void closeLog() {
+		allowE = false;
+		allowW = false;
+		allowI = false;
+		allowD = false;
+		allowV = false;
+		allowWtf = false;
+		allowJson = false;
+	}
+	
+	private static String generateTag(StackTraceElement caller) {
+		String tag = "%s.%s(L:%d)";
+		String callerClazzName = caller.getClassName();
+		callerClazzName = callerClazzName.substring(callerClazzName
+				.lastIndexOf(".") + 1);
+		tag = String.format(tag, callerClazzName, caller.getMethodName(),
+				caller.getLineNumber());
+		tag = TextUtils.isEmpty(customTagPrefix) ? tag : customTagPrefix + ":"
+				+ tag;
+		return tag;
+	}
+
+	public static StackTraceElement getCallerStackTraceElement() {
+		return Thread.currentThread().getStackTrace()[4];
+	}
+
+	public static void d(Object content) {
+		if (!allowD)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.d(tag, String.valueOf(content));
+	}
+
+	public static void d(Object content, Throwable tr) {
+		if (!allowD)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.d(tag, String.valueOf(content), tr);
+	}
+
+	public static void e(Object content) {
+		if (!allowE)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.e(tag, String.valueOf(content));
+	}
+
+	public static void e(Object content, Throwable tr) {
+		if (!allowE)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.e(tag, String.valueOf(content), tr);
+	}
+
+	public static void i(Object content) {
+		if (!allowI)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.i(tag, String.valueOf(content));
+	}
+
+	public static void i(Object content, Throwable tr) {
+		if (!allowI)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.i(tag, String.valueOf(content), tr);
+	}
+
+	public static void v(Object content) {
+		if (!allowV)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.v(tag, String.valueOf(content));
+	}
+
+	public static void v(Object content, Throwable tr) {
+		if (!allowV)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.v(tag, String.valueOf(content), tr);
+	}
+
+	public static void w(Object content) {
+		if (!allowW)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.w(tag, String.valueOf(content));
+	}
+
+	public static void w(Object content, Throwable tr) {
+		if (!allowW)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.w(tag, String.valueOf(content), tr);
+	}
+
+	public static void w(Throwable tr) {
+		if (!allowW)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.w(tag, tr);
+	}
+
+	public static void wtf(Object content) {
+		if (!allowWtf)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.wtf(tag, String.valueOf(content));
+	}
+
+	public static void wtf(Object content, Throwable tr) {
+		if (!allowWtf)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.wtf(tag, String.valueOf(content), tr);
+	}
+
+	public static void wtf(Throwable tr) {
+		if (!allowWtf)
+			return;
+		StackTraceElement caller = getCallerStackTraceElement();
+		String tag = generateTag(caller);
+		Log.wtf(tag, tr);
+	}
+
+	public static void json(String content) {
+		if (!allowJson)
+			return;
+		StackTraceElement caller = Thread.currentThread().getStackTrace()[3];
+        String tag = generateTag(caller);
+		JsonLog.printJson(tag, content);
+	}
+}
+
+class JsonLog {
+
+	public static final String LINE_SEPARATOR = System
+			.getProperty("line.separator");
+	public static final int JSON_INDENT = 4;
+
+	public static void printJson(String tag, String msg) {
+
+		if (isEmpty(msg)) {
+			MLog.e("the json is empty");
+			return;
+		}
+
+		String message = "";
+
+		try {
+			if (msg.startsWith("{")) {
+				JSONObject jsonObject = new JSONObject(msg);
+				message = jsonObject.toString(JSON_INDENT);
+			} else if (msg.startsWith("[")) {
+				JSONArray jsonArray = new JSONArray(msg);
+				message = jsonArray.toString(JSON_INDENT);
+			} else {
+				message = msg;
+			}
+		} catch (JSONException e) {
+			message = msg;
+		}
+
+		printLine(tag, true);
+		String[] lines = message.split(LINE_SEPARATOR);
+		for (String line : lines) {
+			Log.i(tag, "│ " + line);
+		}
+		printLine(tag, false);
+	}
+
+	public static boolean isEmpty(String line) {
+		return TextUtils.isEmpty(line) || line.equals("\n")
+				|| line.equals("\t") || TextUtils.isEmpty(line.trim());
+	}
+
+	public static void printLine(String tag, boolean isTop) {
+		if (isTop) {
+			Log.i(tag,"┌──────────────────────────────────JSON──────────────────────────────────────");
+		} else {
+			Log.i(tag,"└──────────────────────────────────JSON──────────────────────────────────────");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+####MLog工具简介
+
+1. 本工具基于xUtils中的LogUtils实现，整合了KLog中将JOSN数据格式化打印的功能
+2. 只有一个Java类，简单小巧，功能强大
+3. Tag自动产生(customTagPrefix、类名、方法名、行号)，打印方便
+4. 支持修改customTagPrefix来自定义Tag，默认""
+5. 支持打印任意Object类，null打印字符串"null"，否则打印toString
+
+
+
+####使用方法
+1. 将MLog.java拷贝到自己的项目中
+2. 修改package com.ssyijiu.mlog为自己的包名
+3. 修改customTagPrefix变量为自己打印Log的Tag，默认""
+
+			
+		MLog.v("ssyijiu");
+		MLog.d("ssyijiu");
+		MLog.i("ssyijiu");
+		MLog.w("ssyijiu");
+		MLog.e("ssyijiu");
+		MLog.json(json字符串);
+
+
+####联系作者
+* Email: lxmyijiu@163.com
+* V: ssyijiu11
+
+
